8b14edb13cba980cd10f3a67e800e43195bc8568
Alexander Grothe <seahawk1986@gmx.de>
2026-03-28 22:38:00 +0100

    add adaptions for 800x480

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,36 +1,39 @@
 <template>
   <v-app>
-    <v-dialog
-      v-model="isInitializing"
-      fullscreen
-    >
-      <v-container
-        class="fill-height ma-0 pa-0 ga-0"
-        fluid
-        no-gutters
-      >
-        <v-row
-          align="center"
-          justify="start"
-          no-gutters
-        >
-          <v-col
-            cols="12"
-            md="4"
-            no-gutters
-            sm="8"
-          >
-            <v-sheet class="d-flex justify-center align-center ma-0 pa-0 ga-0" height="100vh" width="100vw">
+    <v-dialog v-model="isInitializing" fullscreen>
+      <v-container class="fill-height ma-0 pa-0 ga-0" fluid no-gutters>
+        <v-row align="center" justify="start" no-gutters>
+          <v-col cols="12" md="4" no-gutters sm="8">
+            <v-sheet
+              class="d-flex justify-center align-center ma-0 pa-0 ga-0"
+              height="100vh"
+              width="100vw"
+            >
               <v-container>
                 <v-row align="center">
-                  <v-col class="d-flex justify-center" cols="12">
-                    <CurrentTime class="text-h1" />
+                  <v-col
+                    class="d-flex justify-center"
+                    cols="12"
+                  >
+                    <CurrentTime
+                      class="text-h1"
+                    />
                   </v-col>
-                  <v-col class="d-flex justify-center text-xs-center text-h2" cols="12">
+                  <v-col
+                    class="d-flex justify-center text-xs-center text-h2"
+                    cols="12"
+                  >
                     Waiting for osd2web …
                   </v-col>
-                  <v-col class="d-flex justify-center" cols="12">
-                    <v-progress-circular color="primary" :indeterminate="true" size="64" />
+                  <v-col
+                    class="d-flex justify-center"
+                    cols="12"
+                  >
+                    <v-progress-circular
+                      color="primary"
+                      :indeterminate="true"
+                      size="64"
+                    />
                   </v-col>
                 </v-row>
               </v-container>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,121 +1,128 @@
 <template>
-  <v-card
-    v-if="store.DiashowData?.active === 1"
-  >
-    <v-card-title>
-      {{ store.DiashowData?.title || '' }} ({{ store.DiashowData?.width }}x{{ store.DiashowData?.height }})
-    </v-card-title>
-    <v-card-text class="ma-0 ga-0 pa-0">
-      <v-img class="align-self-center" :src="`http://{{ location.hostname }}:4444/${store.DiashowData?.filename}`" /> <!-- TODO: this needs to explicitly point to the osd2web port only if the skin is not serverd by osd2web -->
-    </v-card-text>
-  </v-card>
-  <v-card
-    v-else-if="store.ReplayControl?.active === 0"
-    :border="false"
-    class="ma-0 pa-0"
-    elevation="0"
-    height="85vh"
-    rounded="0"
-    variant="flat"
-    width="100%"
-  >
-    <v-card-title v-if="store.LiveTvData" class="pa-2">
-      <v-sheet class="pa-2 ma-2 text-h2">
-        {{ store.LiveTvData.channel.channelnumber }}
-        <v-divider :vertical="true" />
-        {{ store.LiveTvData.channel.channelname }}
-      </v-sheet>
-      <v-progress-linear
-        v-if="store.LiveTvData.present.starttime <= store.LiveTvData.present.seen"
-        color="primary"
-        height="20"
-        :max="store.LiveTvData.present.duration"
-        :model-value="(store.LiveTvData.present.endtime - store.LiveTvData.present.seen)"
-      />
-    </v-card-title>
-    <v-card-text>
-      <v-card
-        v-if="store.LiveTvData"
+    <v-card v-if="store.DiashowData?.active === 1">
+        <v-card-title>
+            {{ store.DiashowData?.title || '' }} ({{
+                store.DiashowData?.width
+            }}x{{ store.DiashowData?.height }})
+        </v-card-title>
+        <v-card-text class="ma-0 ga-0 pa-0">
+            <v-img
+                class="align-self-center"
+                :src="`http://{{ location.hostname }}:4444/${store.DiashowData?.filename}`"
+            />
+            <!-- TODO: this needs to explicitly point to the osd2web port only if the skin is not serverd by osd2web -->
+        </v-card-text>
+    </v-card>
+    <v-card
+        v-else-if="store.ReplayControl?.active === 0"
         :border="false"
         class="ma-0 pa-0"
         elevation="0"
+        height="85vh"
         rounded="0"
         variant="flat"
         width="100%"
-      >
-        <TvEvent :current-event-data="store.LiveTvData?.present" />
-        <TvEvent :current-event-data="store.LiveTvData?.following" />
+    >
+        <v-card-title v-if="store.LiveTvData" class="pa-2">
+            <v-sheet class="pa-2 ma-2 text-h2 text-display-small">
+                {{ store.LiveTvData.channel.channelnumber }}
+                <v-divider :vertical="true" />
+                {{ store.LiveTvData.channel.channelname }}
+            </v-sheet>
+            <v-progress-linear
+                v-if="
+                    store.LiveTvData.present.starttime <=
+                    store.LiveTvData.present.seen
+                "
+                color="primary"
+                height="20"
+                :max="store.LiveTvData.present.duration"
+                :model-value="
+                    store.LiveTvData.present.endtime -
+                    store.LiveTvData.present.seen
+                "
+            />
+        </v-card-title>
+        <v-card-text>
+            <v-card
+                v-if="store.LiveTvData"
+                :border="false"
+                class="ma-0 pa-0"
+                elevation="0"
+                rounded="0"
+                variant="flat"
+                width="100%"
+            >
+                <TvEvent :current-event-data="store.LiveTvData?.present" />
+                <TvEvent :current-event-data="store.LiveTvData?.following" />
+            </v-card>
+        </v-card-text>
+        <!-- TODO: add channellogo -->
+    </v-card>
+    <v-card
+        v-else-if="store.ReplayControl?.active === 1"
+        :border="false"
+        class="ma-0 pa-0"
+        elevation="0"
+        height="85vh"
+        rounded="0"
+        variant="flat"
+        width="100%"
+    >
+        <v-card-title class="pa-2">
+            <v-sheet class="pa-2 ma-2 text-h2 text-display-small">
+                {{ store.ReplayData?.basename }}
+            </v-sheet>
+            <v-progress-linear
+                color="primary"
+                height="20"
+                :max="store.ReplayControl?.total"
+                :model-value="store.ReplayControl?.current"
+            />
+        </v-card-title>
+        <v-card-text class="text-h3 pa-2 ma-2 text-display-small" height="50vh">
+            {{ store.ReplayData?.event?.shorttext }}
+        </v-card-text>
 
-      </v-card>
-    </v-card-text>
-    <!-- TODO: add channellogo -->
-  </v-card>
-  <v-card
-    v-else-if="store.ReplayControl?.active === 1"
-    :border="false"
-    class="ma-0 pa-0"
-    elevation="0"
-    height="85vh"
-    rounded="0"
-    variant="flat"
-    width="100%"
-  >
-    <v-card-title class="pa-2">
-      <v-sheet class="pa-2 ma-2 text-h2">
-        {{ store.ReplayData?.basename }}
-      </v-sheet>
-      <v-progress-linear
-        color="primary"
-        height="20"
-        :max="store.ReplayControl?.total"
-        :model-value="store.ReplayControl?.current"
-      />
-    </v-card-title>
-    <v-card-text class="text-h3 pa-2 ma-2" height="50vh">
-      {{ store.ReplayData?.event?.shorttext }}
-    </v-card-text>
-
-    <v-card-text class="text-h5 pa-2 ma-2">
-      <!-- {{ progress }}/{{ duration }} -->
-      <div class="overflow-auto" height="50vh">
-        {{ store.ReplayData?.info?.description }}
-      </div>
-    </v-card-text>
-  </v-card>
+        <v-card-text class="text-h5 pa-2 ma-2 text-display-small">
+            <!-- {{ progress }}/{{ duration }} -->
+            <div class="overflow-auto" height="50vh">
+                {{ store.ReplayData?.info?.description }}
+            </div>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script lang="ts" setup>
-  import TvEvent from '@/components/TvEvent.vue'
-  import { useAppStore } from '@/stores/app'
-  const store = useAppStore()
+import TvEvent from '@/components/TvEvent.vue';
+import { useAppStore } from '@/stores/app';
+const store = useAppStore();
 
-  // function getDuration (timeDeltaSeconds: number): string {
-  //   const hours = Math.trunc(timeDeltaSeconds / 3600)
-  //   const hoursString = hours.toString().padStart(2, '0')
-  //   const minutes = Math.trunc((timeDeltaSeconds - (3600 * hours)) / 60)
-  //   const seconds = (timeDeltaSeconds % 60)
-  //   const secondsString = seconds.toString().padStart(2, '0')
-  //   const minutesString = minutes.toString().padStart(2, '0')
-  //   return `${hoursString}:${minutesString}:${secondsString}`
-  // }
+// function getDuration (timeDeltaSeconds: number): string {
+//   const hours = Math.trunc(timeDeltaSeconds / 3600)
+//   const hoursString = hours.toString().padStart(2, '0')
+//   const minutes = Math.trunc((timeDeltaSeconds - (3600 * hours)) / 60)
+//   const seconds = (timeDeltaSeconds % 60)
+//   const secondsString = seconds.toString().padStart(2, '0')
+//   const minutesString = minutes.toString().padStart(2, '0')
+//   return `${hoursString}:${minutesString}:${secondsString}`
+// }
 
-  // const duration = computed(() => {
-  //   if (store.ReplayControl?.total) {
-  //     return getDuration(store.ReplayControl?.total)
-  //   } else {
-  //     return 'unknown'
-  //   }
-  // })
+// const duration = computed(() => {
+//   if (store.ReplayControl?.total) {
+//     return getDuration(store.ReplayControl?.total)
+//   } else {
+//     return 'unknown'
+//   }
+// })
 
-  // const progress = computed(() => {
-  //   if (store.ReplayControl?.current) {
-  //     return getDuration(store.ReplayControl.current)
-  //   } else {
-  //     return 'unknown'
-  //   }
-  // })
+// const progress = computed(() => {
+//   if (store.ReplayControl?.current) {
+//     return getDuration(store.ReplayControl.current)
+//   } else {
+//     return 'unknown'
+//   }
+// })
 </script>
 
-<style>
-
-</style>
+<style></style>
